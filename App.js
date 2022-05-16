dd4c4171a5e9a0f0d3962f6e64907e3874253ad4
Ying Liu <yingliu4203@gmail.com>
2022-05-16 12:47:04 -0700

    add ppt and home screen

--- App.js
+++ App.js
@@ -1,16 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View, FlatList } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
-import CoinItem from './src/components/CoinItem';
-import cryptocurrencies from './assets/data/cryptocurrencies.json';
+import HomeScreen from './src/screens/HomeScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <FlatList
-        data={cryptocurrencies}
-        renderItem={({ item }) => <CoinItem marketCoin={item} />}
-      />
+      <HomeScreen />
       <StatusBar style="light" />
     </View>
   );
---
+++ src/screens/HomeScreen/index.jsx
@@ -0,0 +1,18 @@
+import { View, Text, FlatList } from 'react-native';
+import React from 'react';
+
+import CoinItem from '../../components/CoinItem';
+import cryptocurrencies from '../../../assets/data/cryptocurrencies.json';
+
+const HomeScreen = () => {
+  return (
+    <View>
+      <FlatList
+        data={cryptocurrencies}
+        renderItem={({ item }) => <CoinItem marketCoin={item} />}
+      />
+    </View>
+  );
+};
+
+export default HomeScreen;
